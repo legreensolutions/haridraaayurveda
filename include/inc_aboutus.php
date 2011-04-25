b69b2c76c90a6a5d18d30c0345c1b26b18967921
LeGreenSolutions <legreensolutions@gmail.com>
2011-04-25 15:12:00 +0530

    content aboutus added

--- include/inc_aboutus.php
+++ include/inc_aboutus.php
@@ -4,13 +4,33 @@
  * and open the template in the editor.
  */
 $head_beyond_healing = 'Beyond Healing';
-$left_beyond_healing = 'LEFT Beyond Healing';
-$right_beyond_healing = 'RIGHT Beyond Healing';
+$left_beyond_healing = 'Haridraa Ayurveda is relentless endeavor towards helping each of its patients attain a disease free life that will be physically, emotionally and spiritually satisfying. This is achieved by providing a holistic health care service comprising the combined effectiveness of Ayurveda with several traditional techniques including Yoga, Pranayama, Meditation etc
+
+We, at Haridraa Ayurveda, strongly believe that while Ayurveda has a long tradition and history, only rational application and sincere administration will enable the successful treatment and cure of almost all the major health problems
+facing modern world. Having understood this reality, Haridraa Ayurveda provides a comprehensive facility for earnest Ayurvedic health care.
+';
+$right_beyond_healing = 'Ayurveda is a truly effective and complete medical system capable of providing answers to any and all the medical crisis challenging modern man in his ultra modern world. KERALA, fortunately has been able to retain a tradition of Ayurveda that has not been completely diluted by the intrusions of contemporary culture and medicine and there is still a large population here that relies on Ayurveda for most of their medical needs.
+
+Welcome to HARIDRAA Ayurveda. A centre of excellence providing tailor made treatments and wellness packages rooted strongly in principles of Ayurveda and carefully designed to suit the needs of each individual patient or customer.';
 
 
 $head_treatments_available = 'Treatments Available';
-$left_treatments_available = 'LEFT Treatments Available';
-$right_treatments_available = 'RIGHT Treatments Available';
+$left_treatments_available = 'Ahyangam(General Body Massage)
+Swedanam(Steam Bath)
+Nasyam
+Pizhichil(Rejuvenation Therapy)
+Sirodhara
+Udwarthanam(Fat Reduction)
+Potala Swedanam(Kizhi)
+Patra Rotala Swedanam
+Njavara Kizhi
+';
+$right_treatments_available = 'Weight Reduction Treatments
+Stress Relieving Treatments
+Detoxification Treatments
+Rejuvination Treatments
+Anti Aging Treatments
+Wellness Treatments';
 ?>
 
 
