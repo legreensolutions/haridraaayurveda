b0c1ccf9820c61b65d26bcd3f6fa6720a1d59f0b
Pramod G. Meon <pramodgmenon@gmail.com>
2011-06-23 12:22:19 +0530

    slide,gallery images, news layout

--- include/page_conf/index.php
+++ include/page_conf/index.php
@@ -4,16 +4,13 @@
 $index_head = $this->gsconf->get_conf(CONF_TYPE_TEXT,'index_head','index','Ayurveda at its best!');
 
 
-$index_top_cont = $this->gsconf->get_conf(CONF_TYPE_TEXT,'index_top_content','index',"'HARDRAA' means turmeric in English. It can remove toxic elements from the human body. Here at 'HARIDRAA AYURVEDA'..... we eliminates diseases with the help of Ayurveda. 'HARIDRAA AYURVEDA' founded by four doctors hailing from traditional Ayurvedic families, that who are specialized in the different branches of ayurveda. Their sole aim is to provide good quality of Ayurvedic medical treatments to their patients. Now 'HARIDRAA' has diversified their activities by opening branches at Vennala, Vyttila and Perumbavoor in Ernakulam District.
-
-Ayurveda is a truly effective and complete medical system capable of providing answers to any and all the medical crisis challenging modern man in his ultra modern world.");
+$index_top_cont = $this->gsconf->get_conf(CONF_TYPE_TEXT,'index_top_content','index',"'HARDRAA' means turmeric in English. It can remove toxic elements from the human body. Here at 'HARIDRAA AYURVEDA'..... we eliminates diseases with the help of Ayurveda. 'HARIDRAA AYURVEDA' founded by four doctors hailing from traditional Ayurvedic families, that who are specialized in the different branches of ayurveda. Their sole aim is to provide good quality of Ayurvedic medical treatments to their patients. Now 'HARIDRAA' has diversified their activities by opening branches at Vennala, Vyttila and Perumbavoor in Ernakulam District.");
 
 $index_top_image = $this->gsconf->get_conf(CONF_TYPE_TEXT,'index_top_image','index','<IMG src="/images/home/home_top.jpg" alt="Ayurveda">');
 
 $index_bottom_image = $this->gsconf->get_conf(CONF_TYPE_TEXT,'index_bottom_image','index','<IMG src="/images/home/home_bottom.jpg" alt="Ayurveda">');
 
-$index_bottom_cont = $this->gsconf->get_conf(CONF_TYPE_TEXT,'index_bottom_content','index',"KERALA, fortunately has been able to retain a tradition of Ayurveda that has not been completely diluted by the intrusions of contemporary culture and medicine and there is still a large population here that relies on Ayurveda for most of their medical needs. Founded on the belief that Ayurveda is comprehensive health care system, we are proud to present an Ayurvedic treatment and research center catering to the seeking individual.
-
+$index_bottom_cont = $this->gsconf->get_conf(CONF_TYPE_TEXT,'index_bottom_content','index',"
 Welcome to HARIDRAA Ayurveda - an Ayurvedic Treatment and Research center, at Lenin Center, Vennala, Ernakulam. A centre of excellence providing tailor made treatments and wellness packages rooted strongly in principles of Ayurveda and carefully designed to suit the needs of each individual patient or customer.");
 
 
