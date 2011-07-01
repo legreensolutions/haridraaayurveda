06b27a0cbbcee89f56d331c54d54862a43357ae3
Pramod G. Meon <pramodgmenon@gmail.com>
2011-07-01 13:15:19 +0530

    typo

--- include/page_conf/index.php
+++ include/page_conf/index.php
@@ -4,7 +4,7 @@
 $index_head = $this->gsconf->get_conf(CONF_TYPE_TEXT,'index_head','index','Ayurveda at its best!');
 
 
-$index_top_cont = $this->gsconf->get_conf(CONF_TYPE_TEXT,'index_top_content','index',"'HARDRAA' means turmeric in English. It can remove toxic elements from the human body. Here at 'HARIDRAA AYURVEDA'..... we eliminates diseases with the help of Ayurveda. 'HARIDRAA AYURVEDA' founded by four doctors hailing from traditional Ayurvedic families, that who are specialized in the different branches of ayurveda. Their sole aim is to provide good quality of Ayurvedic medical treatments to their patients. Now 'HARIDRAA' has diversified their activities by opening branches at Vennala, Vyttila and Perumbavoor in Ernakulam District.");
+$index_top_cont = $this->gsconf->get_conf(CONF_TYPE_TEXT,'index_top_content','index',"'HARIDRAA' means turmeric in English. It can remove toxic elements from the human body. Here at 'HARIDRAA AYURVEDA'..... we eliminates diseases with the help of Ayurveda. 'HARIDRAA AYURVEDA' founded by four doctors hailing from traditional Ayurvedic families, that who are specialized in the different branches of ayurveda. Their sole aim is to provide good quality of Ayurvedic medical treatments to their patients. Now 'HARIDRAA' has diversified their activities by opening branches at Vennala, Vyttila and Perumbavoor in Ernakulam District.");
 
 $index_top_image = $this->gsconf->get_conf(CONF_TYPE_TEXT,'index_top_image','index','<IMG src="/images/home/home_top.jpg" alt="Ayurveda">');
 
